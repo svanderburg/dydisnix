671db6acbb782404dc45b8b642887e13660a237b
Sander van der Burg <svanderburg@gmail.com>
2018-04-27 00:19:40 +0200

    Speed up tests by disabling substitutes

--- release.nix
+++ release.nix
@@ -67,7 +67,14 @@ let
               {
                 virtualisation.writableStore = true;
                 virtualisation.pathsInNixDB = [ pkgs.stdenv pkgs.perlPackages.ArchiveCpio pkgs.busybox ];
-                
+
+                # We can't download any substitutes in a test environment. To make tests
+                # faster, we disable substitutes so that Nix does not waste any time by
+                # attempting to download them.
+                nix.extraOptions = ''
+                  substitute = false
+                '';
+
                 environment.systemPackages = [ disnix dydisnix pkgs.stdenv ] ++ pkgs.libxml2.all ++ pkgs.libxslt.all;
               };
           };
